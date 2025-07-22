77d9dbbf258e45e4f1994a00d9a7dada259b225c
Black <smarterss2018@gmail.com>
2025-07-22 10:27:23 +0800

    Add turbulence around the centre

--- SphereG/core/generator.py
+++ SphereG/core/generator.py
@@ -1,8 +1,9 @@
 import pygame
+import random
 from SphereG.utils.step import step
 from SphereG.utils.remap import remap
 
-def generate(screen: pygame.Surface) -> pygame.Surface:
+def generate(screen: pygame.Surface, radius: float = 0.20) -> pygame.Surface:
     surface = pygame.Surface((screen.get_width(), screen.get_height()))
 
     # Divide the world into sky and earth
@@ -12,5 +13,15 @@ def generate(screen: pygame.Surface) -> pygame.Surface:
                 surface.set_at((x, y), pygame.Color("white"))
             else:
                 surface.set_at((x, y), pygame.Color("black"))
+
+    # Add turbulence around the centre at y = 0 based on radius
+    radius *= screen.get_height()
+
+    top_left = (0, screen.get_height() / 2 - radius)
+
+    pxarray = pygame.surfarray.pixels2d(surface.subsurface(pygame.Rect(top_left, (screen.get_width(), radius * 2))))
+
+    for row in pxarray:
+        random.shuffle(row)
 
     return surface
